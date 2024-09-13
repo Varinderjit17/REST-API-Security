9d18ab55ede4a2c707a2864a9e37bcfc481d34a6
rupi <varinderjit17@gmail.com>
2024-09-13 15:34:03 +1200

    Manual security configuration

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,9 +1,46 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails rupi = User.builder()
+                .username("rupi")
+                .password("{noop}test")
+                .roles("EMPLOYEE", "ADMIN")
+                .build();
 
+        UserDetails aish = User.builder()
+                .username("aish")
+                .password("{noop}aish")
+                .roles("EMPLOYEE", "ADMIN", "MANAGER")
+                .build();
 
+        return new InMemoryUserDetailsManager(rupi, aish);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
